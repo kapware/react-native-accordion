693558926ebcb16551b61dfa68be7ba2a88f8760
Pawel Kapala <github@kapware.com>
2017-11-30 17:58:28 +0100

    Fixed reactCreateClass and PropTypes with recent react

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
-import React, { PropTypes } from 'react';
+import React from 'react';
 import tweenState from 'react-tween-state';
+import PropTypes from 'prop-types';
+import reactCreateClass from 'create-react-class';
 
 import {
   StyleSheet,
@@ -10,19 +12,19 @@ import {
   Text
 } from 'react-native';
 
-var Accordion = React.createClass({
+var Accordion = reactCreateClass({
   mixins: [tweenState.Mixin],
 
   propTypes: {
-    activeOpacity: React.PropTypes.number,
-    animationDuration: React.PropTypes.number,
-    content: React.PropTypes.element.isRequired,
-    easing: React.PropTypes.string,
-    expanded: React.PropTypes.bool,
-    header: React.PropTypes.element.isRequired,
-    onPress: React.PropTypes.func,
-    underlayColor: React.PropTypes.string,
-    style: React.PropTypes.object
+    activeOpacity: PropTypes.number,
+    animationDuration: PropTypes.number,
+    content: PropTypes.element.isRequired,
+    easing: PropTypes.string,
+    expanded: PropTypes.bool,
+    header: PropTypes.element.isRequired,
+    onPress: PropTypes.func,
+    underlayColor: PropTypes.string,
+    style: PropTypes.object
   },
 
   getDefaultProps() {
